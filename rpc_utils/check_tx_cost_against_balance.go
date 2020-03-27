f435059291e4d4b368dae80725af006baf905371
George Carder <george@archon.cloud>
2020-03-27 12:10:20 -0700

    check_tx_cost..

---
+++ rpc_utils/check_tx_cost_against_balance.go
@@ -0,0 +1,23 @@
+package rpc_utils
+
+import (
+	"math/big"
+)
+
+func CheckTxCostAgainstBalance(amount, gasLimit uint64, address [20]byte) (accountHasEnough bool, balance, totalCost big.Int, err error) {
+	bBalance, err := GetBalance_byteAddressToBigInt(address)
+	if err != nil {
+		return false, bBalance, *new(big.Int), err
+	}
+	bAmount := new(big.Int)
+	bAmount.SetUint64(amount)
+	bGasLimit := new(big.Int)
+	bGasLimit.SetUint64(gasLimit)
+
+	bTotalCost := bAmount.Add(bAmount, bGasLimit)
+	difference := bBalance.Sub(&bBalance, bTotalCost)
+	if difference.Sign() < 0 {
+		return false, balance, totalCost, nil
+	}
+	return true, bBalance, *bTotalCost, nil
+}
