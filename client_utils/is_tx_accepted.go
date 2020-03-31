514705e0d5e8ec80b25af0d77995afba310fc6ab
George Carder <george@archon.cloud>
2020-03-31 13:28:53 -0700

    comment on is_tx_accepted

--- client_utils/is_tx_accepted.go
+++ client_utils/is_tx_accepted.go
@@ -1,5 +1,7 @@
 package client_utils
 
+// Note this is designed for registerSP and proposeUpload tx only. Other txs
+// will not necessarily return correct bool.
 func IsTxAcceptedByBlockchain(txid string) (bool, error) {
 	logs, err := GetTxLogs(txid)
 	if err != nil {
